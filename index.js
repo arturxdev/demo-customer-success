350762919cd7087c2c4210e063a18f60d59350ae
Arturo GUerrero <arturo.guerrero@koibanx.com>
2023-11-10 11:59:16 -0600

    parmas and img

--- index.js
+++ index.js
@@ -5,42 +5,46 @@ const port = 3000;
 const pug = require("pug");
 const path = require("path");
 const bodyParser = require("body-parser");
-require('dotenv').config()
+require("dotenv").config();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "assets")));
 
-app.get("/", async (req, res) => {
-  console.log(req.query);
+app.get("/:id", async (req, res) => {
+  console.log(req.query, req.params);
+  let title = "Default";
+  req.params.id == "neomoon" ? title="Neomoon" : null;
+  req.params.id == "credibanco" ? title="Credibanco" : null;
+  req.params.id == "marcospaz" ? title="Marcos Paz" : null;
   if (req.query.value) {
+    let data = JSON.stringify({
+      "dataSource": "dev-common-uy-01",
+      "database": "customer-success",
+      "collection": "emails",
+      document: {
+        value: req.query.value,
+      },
+    });
+
+    let config = {
+      method: "post",
+      maxBodyLength: Infinity,
+      url:
+        "https://sa-east-1.aws.data.mongodb-api.com/app/data-gtkso/endpoint/data/v1/action/insertOne",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Request-Headers": "*",
+        "api-key": process.env.MONGO_KEY,
+      },
+      data: data,
+    };
+
+    const result = await axios.request(config);
+    console.log(result.data);
   }
-  let data = JSON.stringify({
-    "dataSource": "dev-common-uy-01",
-    "database": "customer-success",
-    "collection": "emails",
-    document: {
-      value: req.query.value,
-    },
-  });
-
-  let config = {
-    method: "post",
-    maxBodyLength: Infinity,
-    url:
-      "https://sa-east-1.aws.data.mongodb-api.com/app/data-gtkso/endpoint/data/v1/action/insertOne",
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Request-Headers": "*",
-      "api-key":process.env.MONGO_KEY,
-    },
-    data: data,
-  };
-
-  const result = await axios.request(config);
-  console.log(result.data);
-  res.render("index");
+  res.render("index",{title});
 });
 app.post("/stats", async ({ body }, res) => {
   let document = {
@@ -65,7 +69,7 @@ app.post("/stats", async ({ body }, res) => {
     headers: {
       "Content-Type": "application/json",
       "Access-Control-Request-Headers": "*",
-      "api-key":process.env.MONGO_KEY,
+      "api-key": process.env.MONGO_KEY,
     },
     data: data,
   };
